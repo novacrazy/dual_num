b15b044bddfc770732a700fa2fe26087f74dffa5
Christopher Rabotin <christopher.rabotin@gmail.com>
2018-06-21 01:01:47 -0600

    Now using hyperdual space
    Must now extend this to support non square matrices

--- src/differentials.rs
+++ src/differentials.rs
@@ -11,45 +11,52 @@ where
     f(Dual::new(x, T::one())).dual()
 }
 
-/// Computes the gradiant of the provided function with a preliminary time parameter.
+/// Computes the state and the gradiant of the provided function with a preliminary time parameter.
 ///
 /// # How to read this signature:
 /// Let `t` be a parameter, `x` be a state vector and `f` be a function whose gradient is seeked.
-/// Calling `nabla` will return ∇f(t, x).
-pub fn nabla_t<T: Real + Num, N: DimName, F>(t: T, x: VectorN<T, N>, f: F) -> MatrixMN<T, N, N>
+/// Calling `nabla_t` will return a tuple of ( f(t, x), ∇f(t, x) ).
+pub fn nabla_t<T: Real + Num, N: DimName, F>(t: T, x: VectorN<T, N>, f: F) -> (VectorN<T, N>, MatrixMN<T, N, N>)
 where
-    F: Fn(T, &VectorN<Dual<T>, N>) -> VectorN<Dual<T>, N>,
+    F: Fn(T, &MatrixMN<Dual<T>, N, N>) -> MatrixMN<Dual<T>, N, N>,
     DefaultAllocator: Allocator<Dual<T>, N> + Allocator<Dual<T>, N, N> + Allocator<usize, N> + Allocator<T, N> + Allocator<T, N, N>,
 {
-    let mut grad_as_slice = Vec::with_capacity(N::dim() * N::dim());
+    // Create a Matrix for the hyperdual space
+    let mut hyperdual_space = MatrixMN::<Dual<T>, N, N>::zeros();
 
-    // "Simulate" a hyperdual space of size N::dim()
-    for v_i in 0..N::dim() {
-        let mut dual_x = VectorN::<Dual<T>, N>::zeros();
-
-        for i in 0..N::dim() {
-            dual_x[(i, 0)] = Dual::new(x[(i, 0)], if v_i == i { T::one() } else { T::zero() });
+    for i in 0..N::dim() {
+        let mut v_i = VectorN::<Dual<T>, N>::zeros();
+        for j in 0..N::dim() {
+            v_i[(j, 0)] = Dual::new(x[(j, 0)], if i == j { T::one() } else { T::zero() });
         }
-
-        let df_di = f(t, &dual_x);
-
-        for i in 0..N::dim() {
-            grad_as_slice.push(df_di[(i, 0)].dual());
-        }
+        hyperdual_space.set_column(i, &v_i);
     }
 
-    MatrixMN::<T, N, N>::from_column_slice(&grad_as_slice)
+    let state_n_grad = f(t, &hyperdual_space);
+    // Extract the dual part
+    let mut state = VectorN::<T, N>::zeros();
+    let mut grad = MatrixMN::<T, N, N>::zeros();
+    for i in 0..N::dim() {
+        for j in 0..N::dim() {
+            if j == 0 {
+                // The state is duplicated in every column
+                state[(i, j)] = state_n_grad[(i, j)].real();
+            }
+            grad[(i, j)] = state_n_grad[(i, j)].dual();
+        }
+    }
+    (state, grad)
 }
 
-/// Computes the gradiant of the provided function.
+/// Computes the state and gradiant of the provided function.
 ///
 /// # How to read this signature:
 /// Let `x` be a state vector and `f` be a function whose gradient is seeked.
-/// Calling `nabla` will return ∇f(x).
+/// Calling `nabla` will return a tuple of ( f(x), ∇f(x) ).
 #[inline]
-pub fn nabla<T: Real + Num, N: DimName, F>(x: VectorN<T, N>, f: F) -> MatrixMN<T, N, N>
+pub fn nabla<T: Real + Num, N: DimName, F>(x: VectorN<T, N>, f: F) -> (VectorN<T, N>, MatrixMN<T, N, N>)
 where
-    F: Fn(&VectorN<Dual<T>, N>) -> VectorN<Dual<T>, N>,
+    F: Fn(&MatrixMN<Dual<T>, N, N>) -> MatrixMN<Dual<T>, N, N>,
     DefaultAllocator: Allocator<Dual<T>, N> + Allocator<Dual<T>, N, N> + Allocator<usize, N> + Allocator<T, N> + Allocator<T, N, N>,
 {
     nabla_t(T::zero(), x, |_, x| f(x))
--- tests/lib.rs
+++ tests/lib.rs
@@ -5,7 +5,7 @@ extern crate nalgebra as na;
 
 use dual_num::linalg::norm;
 use dual_num::{nabla, nabla_t};
-use na::{Matrix6, U3, Vector3, Vector6};
+use na::{Matrix3x6, Matrix6, U3, U6, Vector3, Vector6};
 
 macro_rules! abs_within {
     ($x:expr, $val:expr, $eps:expr, $msg:expr) => {
@@ -58,17 +58,29 @@ fn test_norm() {
 #[test]
 fn gradient_no_param() {
     // This is an example of the equation of motion gradient for a spacecrate in a two body acceleration.
-    fn eom(state: &Vector6<Dual<f64>>) -> Vector6<Dual<f64>> {
-        let radius = state.fixed_rows::<U3>(0).into_owned();
-        let velocity = state.fixed_rows::<U3>(3).into_owned();
-        let norm = (radius[(0, 0)].powi(2) + radius[(1, 0)].powi(2) + radius[(2, 0)].powi(2)).sqrt();
-        let body_acceleration_f = Dual::from_real(-398_600.4415) / norm.powi(3);
-        let body_acceleration = Vector3::new(
-            radius[(0, 0)] * body_acceleration_f,
-            radius[(1, 0)] * body_acceleration_f,
-            radius[(2, 0)] * body_acceleration_f,
-        );
-        Vector6::from_iterator(velocity.iter().chain(body_acceleration.iter()).cloned())
+    fn eom(state: &Matrix6<Dual<f64>>) -> Matrix6<Dual<f64>> {
+        let radius = state.fixed_slice::<U3, U6>(0, 0).into_owned();
+        let velocity = state.fixed_slice::<U3, U6>(3, 0).into_owned();
+        let mut body_acceleration = Matrix3x6::zeros();
+        for i in 0..3 {
+            let this_norm = norm(&Vector3::new(radius[(0, i)], radius[(1, i)], radius[(2, i)]));
+            let body_acceleration_f = Dual::from_real(-398_600.4415) / this_norm.powi(3);
+            let this_body_acceleration = Vector3::new(
+                radius[(0, i)] * body_acceleration_f,
+                radius[(1, i)] * body_acceleration_f,
+                radius[(2, i)] * body_acceleration_f,
+            );
+            body_acceleration.set_column(i, &this_body_acceleration);
+        }
+        let mut rtn = Matrix6::zeros();
+        for i in 0..6 {
+            if i < 3 {
+                rtn.set_row(i, &velocity.row(i));
+            } else {
+                rtn.set_row(i, &body_acceleration.row(i - 3));
+            }
+        }
+        rtn
     }
 
     let state = Vector6::new(
@@ -79,7 +91,18 @@ fn gradient_no_param() {
         -2.226285193102822,
         1.6467383807226765,
     );
-    let grad = nabla(state, eom);
+    let (fx, grad) = nabla(state, eom);
+
+    let expected_fx = Vector6::new(
+        -3.28878900377057,
+        -2.226285193102822,
+        1.6467383807226765,
+        0.0003488751720191492,
+        -0.0007151349009902908,
+        -0.00027005954128877916,
+    );
+
+    zero_within!((fx - expected_fx).norm(), 1e-16, "f(x) computation is incorrect");
 
     let mut expected = Matrix6::zeros();
     expected[(0, 3)] = 1.0;
@@ -101,18 +124,29 @@ fn gradient_no_param() {
 #[test]
 fn gradient_with_param() {
     // This is an example of the equation of motion gradient for a spacecrate in a two body acceleration.
-    fn eom(_t: f64, state: &Vector6<Dual<f64>>) -> Vector6<Dual<f64>> {
-        let radius = state.fixed_rows::<U3>(0).into_owned();
-        let velocity = state.fixed_rows::<U3>(3).into_owned();
-        let norm = (radius[(0, 0)].powi(2) + radius[(1, 0)].powi(2) + radius[(2, 0)].powi(2)).sqrt();
-        let body_acceleration_f = Dual::from_real(-398_600.4415) / norm.powi(3);
-        let body_acceleration = Vector3::new(
-            radius[(0, 0)] * body_acceleration_f,
-            radius[(1, 0)] * body_acceleration_f,
-            radius[(2, 0)] * body_acceleration_f,
-        );
-
-        Vector6::from_iterator(velocity.iter().chain(body_acceleration.iter()).cloned())
+    fn eom(_t: f64, state: &Matrix6<Dual<f64>>) -> Matrix6<Dual<f64>> {
+        let radius = state.fixed_slice::<U3, U6>(0, 0).into_owned();
+        let velocity = state.fixed_slice::<U3, U6>(3, 0).into_owned();
+        let mut body_acceleration = Matrix3x6::zeros();
+        for i in 0..3 {
+            let this_norm = norm(&Vector3::new(radius[(0, i)], radius[(1, i)], radius[(2, i)]));
+            let body_acceleration_f = Dual::from_real(-398_600.4415) / this_norm.powi(3);
+            let this_body_acceleration = Vector3::new(
+                radius[(0, i)] * body_acceleration_f,
+                radius[(1, i)] * body_acceleration_f,
+                radius[(2, i)] * body_acceleration_f,
+            );
+            body_acceleration.set_column(i, &this_body_acceleration);
+        }
+        let mut rtn = Matrix6::zeros();
+        for i in 0..6 {
+            if i < 3 {
+                rtn.set_row(i, &velocity.row(i));
+            } else {
+                rtn.set_row(i, &body_acceleration.row(i - 3));
+            }
+        }
+        rtn
     }
 
     let state = Vector6::new(
@@ -124,7 +158,18 @@ fn gradient_with_param() {
         1.6467383807226765,
     );
 
-    let grad = nabla_t(0.0, state, eom);
+    let (fx, grad) = nabla_t(0.0, state, eom);
+
+    let expected_fx = Vector6::new(
+        -3.28878900377057,
+        -2.226285193102822,
+        1.6467383807226765,
+        0.0003488751720191492,
+        -0.0007151349009902908,
+        -0.00027005954128877916,
+    );
+
+    zero_within!((fx - expected_fx).norm(), 1e-16, "f(x) computation is incorrect");
 
     let mut expected = Matrix6::zeros();
 
