e250c9592dd55d27b915c61998e2bd1a7e5d7007
Christopher Rabotin <christopher.rabotin@gmail.com>
2018-06-22 20:26:57 -0600

    Renamed nabla* to partial* and extended function docs
    The nabla operator is used for the gradient computation, and a gradient
    is by definition square (contrarly to what I may have written earlier).
    However, Dual numbers may be used to compute any function partial with
    respect to the provided state (from where a hyperdual space is
    generated).

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,8 +4,8 @@ use dual_num::{differentiate, Dual, Float, FloatConst};
 extern crate nalgebra as na;
 
 use dual_num::linalg::norm;
-use dual_num::{nabla, nabla_t};
-use na::{Matrix3x6, Matrix6, U3, U6, Vector3, Vector6};
+use dual_num::{partials, partials_t};
+use na::{Matrix2x6, Matrix3x6, Matrix6, Matrix6x2, U3, U6, Vector3, Vector6};
 
 macro_rules! abs_within {
     ($x:expr, $val:expr, $eps:expr, $msg:expr) => {
@@ -56,7 +56,7 @@ fn test_norm() {
 }
 
 #[test]
-fn gradient_no_param() {
+fn square_gradient_no_param() {
     // This is an example of the equation of motion gradient for a spacecrate in a two body acceleration.
     fn eom(state: &Matrix6<Dual<f64>>) -> Matrix6<Dual<f64>> {
         let radius = state.fixed_slice::<U3, U6>(0, 0).into_owned();
@@ -91,7 +91,7 @@ fn gradient_no_param() {
         -2.226285193102822,
         1.6467383807226765,
     );
-    let (fx, grad) = nabla(state, eom);
+    let (fx, grad) = partials(state, eom);
 
     let expected_fx = Vector6::new(
         -3.28878900377057,
@@ -122,7 +122,7 @@ fn gradient_no_param() {
 }
 
 #[test]
-fn gradient_with_param() {
+fn square_gradient_with_param() {
     // This is an example of the equation of motion gradient for a spacecrate in a two body acceleration.
     fn eom(_t: f64, state: &Matrix6<Dual<f64>>) -> Matrix6<Dual<f64>> {
         let radius = state.fixed_slice::<U3, U6>(0, 0).into_owned();
@@ -158,7 +158,7 @@ fn gradient_with_param() {
         1.6467383807226765,
     );
 
-    let (fx, grad) = nabla_t(0.0, state, eom);
+    let (fx, grad) = partials_t(0.0, state, eom);
 
     let expected_fx = Vector6::new(
         -3.28878900377057,
@@ -188,3 +188,59 @@ fn gradient_with_param() {
 
     zero_within!((grad - expected).norm(), 1e-16, "gradient computation is incorrect");
 }
+/*
+#[test]
+fn nonsquare_gradient_no_param() {
+    // This is an example of the equation of motion gradient for a spacecrate in a two body acceleration.
+    fn sensitivity(state: &Matrix2x6<Dual<f64>>) -> Matrix2x6<Dual<f64>> {
+        panic!("{}", state);
+        let radius = state.fixed_slice::<U3, U6>(0, 0).into_owned();
+        let velocity = state.fixed_slice::<U3, U6>(3, 0).into_owned();
+        let mut body_acceleration = Matrix3x6::zeros();
+        for i in 0..3 {
+            let this_norm = norm(&Vector3::new(radius[(0, i)], radius[(1, i)], radius[(2, i)]));
+            let body_acceleration_f = Dual::from_real(-398_600.4415) / this_norm.powi(3);
+            let this_body_acceleration = Vector3::new(
+                radius[(0, i)] * body_acceleration_f,
+                radius[(1, i)] * body_acceleration_f,
+                radius[(2, i)] * body_acceleration_f,
+            );
+            body_acceleration.set_column(i, &this_body_acceleration);
+        }
+        let mut rtn = Matrix2x6::zeros();
+        // for i in 0..6 {
+        //     if i < 3 {
+        //         rtn.set_row(i, &velocity.row(i));
+        //     } else {
+        //         rtn.set_row(i, &body_acceleration.row(i - 3));
+        //     }
+        // }
+        rtn
+    }
+
+    let rx = Vector6::new(9203.993716, 18450.606914, 7016.410940, -3.769098, 1.255100, 1.644035);
+    let tx = Vector6::new(4849.340233, 360.415547, 4114.752758, -0.026282, 0.353619, 0.000000);
+
+    let (_, grad) = partials(rx - tx, sensitivity);
+
+    /*
+┌                                                                                                                                                 ┐
+│     0.23123905265689662      0.9606180445702461        0.15408268225981                       0                       0                       0 │
+│ -0.00019563292172470923 0.000060817042613472664  0.00008937755133596409     0.23123905265689662      0.9606180445702461        0.15408268225981 │
+└                                                                                                                                                 ┘
+*/
+
+    let mut expected_grad = Matrix2x6::zeros();
+    expected_grad[(0, 0)] = 0.23123905265689662;
+    expected_grad[(0, 1)] = 0.9606180445702461;
+    expected_grad[(0, 2)] = 0.15408268225981;
+    expected_grad[(1, 3)] = -0.00019563292172470923;
+    expected_grad[(1, 4)] = 0.000060817042613472664;
+    expected_grad[(1, 5)] = 0.00008937755133596409;
+    expected_grad[(1, 3)] = 0.23123905265689662;
+    expected_grad[(1, 4)] = 0.9606180445702461;
+    expected_grad[(1, 5)] = 0.15408268225981;
+
+    zero_within!((grad - expected_grad).norm(), 1e-16, "gradient computation is incorrect");
+}
+*/
